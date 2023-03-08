f093577a5f6179a582b83fb5b5ea4ff6eba1e1f8
alexhessami <104582633+alexhessami@users.noreply.github.com>
2023-03-08 08:39:38 -0800

    Update main.go

--- catnip/main.go
+++ catnip/main.go
@@ -8,50 +8,34 @@ import (
 )
 
 func main() {
-
 	//flag creation
-	bytes := 4096
-	bytesSpecify := flag.Int("r", 0, "Specify the number of bytes. '-r # of bytes'")
+	defaultBufferSize := 4096
+	bufferSize := flag.Int("r", defaultBufferSize, "Specify the buffer size. '-r #bytes'")
 	flag.Parse()
-	r := *bytesSpecify
 
-	//if -b flag is not used make it 4096 bytes/second by default
-	if r == 0 {
-		bytes = 4096
-	} else {
-		bytes = *bytesSpecify
-	}
-
-	//Created the input and output streams
+	//create input and output streams
 	input := os.Stdin
 	output := os.Stdout
 
-	//Allocate specified bytes to read from the input stream (4096 by default)
-	buffer := make([]byte, bytes)
+	//create buffer
+	buffer := make([]byte, *bufferSize)
 
-	//Create loop to continuously output data as it comes in
 	for {
-		//Read from the input stream
+		//read from input stream
 		data, err := input.Read(buffer)
-		//error handling
-		//ignoring EOF error since we are continuously reading the file
-		if err != nil && err != io.EOF {
+		if err == io.EOF {
+			break
+		} else if err != nil {
 			panic(err)
 		}
-		if data == 0 {
-			//Wait for one second if no input before continuing
-			time.Sleep(time.Second)
-			break
-		}
 
-		//Write to the output stream
+		//write to output stream
 		_, err = output.Write(buffer[:data])
-		//error handling
 		if err != nil {
 			panic(err)
 		}
 
-		//Wait to limit the rate based off of specified bytes (default 4096)
-		time.Sleep(time.Second / time.Duration(bytes))
+		//wait to limit the rate based on buffer size
+		time.Sleep(time.Second / time.Duration(*bufferSize))
 	}
 }
