cb0c7e0644267d6ebef89060cb6a14738e442082
alexhessami <104582633+alexhessami@users.noreply.github.com>
2023-03-23 19:29:59 -0700

    Update main.go

--- catnip/main.go
+++ catnip/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"os"
 	"time"
@@ -11,42 +12,58 @@ func main() {
 	//flag creation
 	defaultBufferSize := 4096
 	bufferSize := flag.Int("r", defaultBufferSize, "Specify the buffer size. '-r #bytes'")
+	verbose := flag.Bool("v", false, "Enable verbose mode.")
+	help := flag.Bool("help", false, "By default output will be 4096 bytes a second. Use -r # to specify the number of bytes. Use -v for verbose.")
 	flag.Parse()
 
 	//create input and output streams
 	input := os.Stdin
 	output := os.Stdout
 
+	if err := start(input, output, *bufferSize, *verbose, *help); err != nil {
+		panic(err)
+	}
+}
+
+func start(input io.Reader, output io.Writer, bufferSize int, verbose bool, help bool) error {
 	//create buffer
-	buffer := make([]byte, *bufferSize)
+	buffer := make([]byte, bufferSize)
 
 	for {
+		if help {
+			fmt.Println("By default output will be 4096 bytes a second. Use -r # to specify the number of bytes. Use -v for verbose.")
+			break
+		}
+
 		//read from input stream
 		var total int
-		for total < *bufferSize {
-			data, err := input.Read(buffer[total:*bufferSize])
+		for total < bufferSize {
+			data, err := input.Read(buffer[total:bufferSize])
 			if err == io.EOF {
 				break
 			} else if err != nil {
-				panic(err)
+				return err
 			}
 			total += data
+
+			if verbose {
+				fmt.Print(".")
+			}
 		}
 
 		//write to output stream
-		var err error
 		for written := 0; written < total; {
+			var err error
 			data, err := output.Write(buffer[written:total])
 			if err != nil {
-				panic(err)
+				return err
 			}
 			written += data
 		}
-		if err != nil {
-			panic(err)
-		}
 
 		//wait to limit the rate based on buffer size
-		time.Sleep(time.Second / time.Duration(*bufferSize))
+		time.Sleep(time.Second / time.Duration(bufferSize))
 	}
+	return nil
+
 }
